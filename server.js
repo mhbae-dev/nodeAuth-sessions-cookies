ac90045e92f7c0352918c81a9f68003614733672
Myoung <mhbae93@gmail.com>
2022-03-03 04:56:33 +0000

    connected to local instance of mongodb

--- server.js
+++ server.js
@@ -1,12 +1,31 @@
-import express from 'express';
+import express from "express";
+import session from "express-session";
+import mongoose from "mongoose";
 
 const app = express();
 const PORT = 5000;
 
+//DATABASE CONNECTION
+mongoose
+  .connect("mongodb://127.0.0.1:27017/sessions")
+  .then((res) => {
+    console.log("MongoDDB Connected");
+  })
+  .catch((error) => handleError(error));
+
+app.use(
+  session({
+    secret: "key will sign cookie",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
 app.get("/", (req, res) => {
-  res.send('Hello Sessions')
-})
+  console.log(req.session);
+  res.send("Hello Sessions");
+});
 
 app.listen(PORT, () => {
-  console.log(`Server is running on ${PORT}`)
-})
+  console.log(`Server is running on ${PORT}`);
+});
