368bdc9a954d020d2cbbda66a8143f278c1b60e9
Myoung <mhbae93@gmail.com>
2022-03-03 06:08:48 +0000

    user can be saved to databse with password encryption

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 import express from "express";
+import bcrypt from "bcryptjs";
 import session from "express-session";
 import { default as connectMongoDBSession } from "connect-mongodb-session";
 import mongoose from "mongoose";
-import PostUser from "./models/User.js";
-
+import UserModel from "./models/User.js";
 const mongoURI = "mongodb://127.0.0.1:27017/sessions";
 const MongoDBStore = connectMongoDBSession(session);
 
@@ -23,6 +23,9 @@ const store = new MongoDBStore({
   collection: "session",
 });
 
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+
 app.use(
   session({
     secret: "key will sign cookie",
@@ -33,8 +36,43 @@ app.use(
 );
 
 app.get("/", (req, res) => {
-  req.session.isAuth = true;
-  res.send("Hello Sessions");
+  res.render("landing");
+});
+
+app.get("/login", (req, res) => {
+  const error = req.session.error;
+  res.render("login", { err: error });
+});
+
+app.get("/register", (req, res) => {
+  const error = req.session.error;
+  res.render("register", { err: error });
+});
+
+app.post("/register", async (req, res) => {
+  const { username, email, password } = req.body;
+
+  let user = await UserModel.findOne({ email });
+
+  if (user) {
+    req.session.error = "User already exists";
+    return res.redirect("/register");
+  }
+
+  const hashedPsw = await bcrypt.hash(password, 12);
+
+  user = new UserModel({
+    username,
+    email,
+    password: hashedPsw,
+  });
+
+  await user.save();
+  res.redirect("/login");
+});
+
+app.get("/dashboard", (req, res) => {
+  res.render("dashboard");
 });
 
 app.listen(PORT, () => {
